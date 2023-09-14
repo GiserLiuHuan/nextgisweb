783f775a8aea6a53005edf8c16529cf2373c20ea
floatline <poltewski@gmail.com>
2023-09-14 13:02:58 +0300

    Fix extract_meta method name

--- nextgisweb/feature_attachment/util.py
+++ nextgisweb/feature_attachment/util.py
@@ -141,7 +141,7 @@ def attachments_import(resource, filename, *, replace):
                         size=src['info'].file_size,
                     ).persist()
                     obj.fileobj = fileobj.persist()
-                    obj.extract_metadata()
+                    obj.extract_meta()
                     imported += 1
                 else:
                     skipped += 1
